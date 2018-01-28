8536ea338a9fb1e2a6e9dd7f058312a8bd797010
Tyler <millert2420@gmail.com>
2018-01-27 22:47:48 -0700

    Added Inputs

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    
+    this.state = {
+      friends: [],
+      picture: '',
+      name: '',
+    };
+  }
+  render() {
+    return (
+      <div>
+        <span>Picture:</span>
+          <input />
+
+          <span>Name:</span>
+          <input />
+
+          <button>Add Friend</button>
+      </div>
+    );
+  }
+}
+
+export default App;
